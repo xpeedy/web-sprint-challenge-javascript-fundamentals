bc07d0125f90e833636e405243aefef5cdf234fe
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2020-10-16 13:27:41 -0400

    working-lastTask

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -22,7 +22,7 @@ The zoos want to display both the scientific name and the animal name in front o
 */
 const displayNames = [];
 
-zooAnimals.forEach(function(element){
+zooAnimals.forEach((element) => {
   displayNames.push(`name: ${element.animal_name}, scientific: ${element.scientific_name}`)
 })
 console.log(displayNames);
@@ -32,7 +32,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = zooAnimals.map(function(element){
+const lowCaseAnimalNames = zooAnimals.map((element) => {
   return element.animal_name.toLowerCase()
 })
 console.log(lowCaseAnimalNames);
@@ -42,7 +42,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = zooAnimals.filter(function(element){
+const lowPopulationAnimals = zooAnimals.filter((element) => {
   if(element.population < 5){
     return element;
   }
@@ -55,7 +55,7 @@ The zoos need to know their total animal population across the United States. Fi
 
 */
 
-let populationTotal = zooAnimals.reduce(function(counter,element){
+let populationTotal = zooAnimals.reduce((counter,element) => {
   return counter + element.population;
 },0)
 console.log(populationTotal);
@@ -70,6 +70,9 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+let consume = function(a,b,c){
+  return c(a,b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -77,11 +80,21 @@ console.log(populationTotal);
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+let add = function(num1,num2){
+  return num1 + num2;
+}
+let multiply = function(num1,num2){
+  return num1 * num2;
+}
+let greeting = function(name,lastName){
+  return `hello ${name} ${lastName}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
